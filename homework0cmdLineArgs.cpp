a4c8c6309e71e4d6752c4f705b385c29010ce135
Matthew Kobilas <mattkobi789@gmail.com>
2017-09-18 00:18:17 -0400

    Corrected program
    Corrected program to account for escaped characters such as new lines or tabs.

--- homework0cmdLineArgs.cpp
+++ homework0cmdLineArgs.cpp
@@ -5,18 +5,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	fstream inFile;
-	int numChar;
-	string line;
-	for (int i = 0; i < argc; i++) {
-		numChar = 0;
+	ifstream inFile;
+	int size;
+	for (int i = 1; i < argc; i++) {
 		inFile.open(argv[i]);
 		if (inFile.is_open()) {
-			while (getline(inFile, line)) {
-				numChar += line.length();
-			}
+            		inFile.seekg (0, inFile.end);
+            		size = inFile.tellg();
 			inFile.close();
-			cout << argv[i] << " " << numChar << endl;
+			cout << argv[i] << " " << size << endl;
 		}
 		else {
 			cout << argv[i] << " FILE NOT FOUND" << endl;
